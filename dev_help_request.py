47a383234f38af9c77bd8c03461cc3632f753214
QuantumRaven <carpenterrchloe@protonmail.com>
2025-04-22 13:08:26 -0400

    Remove help_request() call at EOF and add description
    Remove help_request() call at EOF as it is no longer relevant. Add
    description for each function.

--- dev_help_request.py
+++ dev_help_request.py
@@ -3,12 +3,18 @@
 import sys, os
 
 def clear_terminal():
+    """Checks what operation system is in use: Windows or Linux.
+    Then, it clears the terminal with the approriate command based
+    on the operation system detected.
+    """
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system("clear")
 
 def bootdev_help_request():
+    """This generates a help request for https://boot.dev/ lessons
+    based on their provided formatting in Discord."""
     lesson_link = input("\nLesson link: ")    
 
     print("\nNow, enter your text (type 'END' on a new line to finish):\n")
@@ -16,4 +22,4 @@ def bootdev_help_request():
     clear_terminal()
     print(f"{lesson_link}\n\n{multiline_input}")
 
-help_request()
+
