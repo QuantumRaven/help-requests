162d65ae7cfed9023f65d19ba4302d962ec5aa70
QuantumRaven <carpenterrchloe@protonmail.com>
2025-04-22 13:24:44 -0400

    Use black for formatting and correct misspelling
    Correction operation to operating for "operation system".

--- dev_help_request.py
+++ dev_help_request.py
@@ -5,7 +5,7 @@ import os
 
 
 def clear_terminal():
-    """Checks what operation system is in use: Windows or Linux.
+    """Checks what operating system is in use: Windows or Linux.
     Then, it clears the terminal with the approriate command based
     on the operation system detected.
     """
@@ -18,6 +18,16 @@ def clear_terminal():
 def bootdev_help_request():
     """This generates a help request for https://boot.dev/ lessons
     based on their provided formatting in Discord."""
+    lesson_link = input("\nLesson link: ")
+
+    print("\nNow, enter your text (type 'END' on a new line to finish):\n")
+    multiline_input = sys.stdin.read()
+    clear_terminal()
+    print(f"{lesson_link}\n\n{multiline_input}")
+
+def theodinproject_help_request():
+    """This generates a help request for https://theodinproject.com/ lessons
+    based on their provided formatting in Discord and website."""
     lesson_link = input("\nLesson link: ")
 
     print("\nNow, enter your text (type 'END' on a new line to finish):\n")
