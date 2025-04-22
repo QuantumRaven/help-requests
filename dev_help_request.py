4db7588e3503282e158040ab7aca26d1758d2ca7
QuantumRaven <carpenterrchloe@protonmail.com>
2025-04-22 13:10:38 -0400

    Add imports on separtae lines
    Give each import its own line for syntactical accuracy. Use python's
    black formatter to reformat code.

--- dev_help_request.py
+++ dev_help_request.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
-import sys, os
+import sys
+import os
+
 
 def clear_terminal():
     """Checks what operation system is in use: Windows or Linux.
     Then, it clears the terminal with the approriate command based
     on the operation system detected.
     """
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
         os.system("clear")
+
 
 def bootdev_help_request():
     """This generates a help request for https://boot.dev/ lessons
     based on their provided formatting in Discord."""
-    lesson_link = input("\nLesson link: ")    
+    lesson_link = input("\nLesson link: ")
 
     print("\nNow, enter your text (type 'END' on a new line to finish):\n")
     multiline_input = sys.stdin.read()
     clear_terminal()
     print(f"{lesson_link}\n\n{multiline_input}")
-
-
