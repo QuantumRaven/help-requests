32349b60487a03aac39034d3e45e145ce3855e65
QuantumRaven <carpenterrchloe@protonmail.com>
2025-04-22 15:46:19 -0400

    Base for GUI application tkinter

---
+++ dev_help_request_gui.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+
+def clear_terminal():
+    """Checks what operating system is in use: Windows or Linux.
+    Then, it clears the terminal with the approriate command based
+    on the operation system detected.
+    """
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def bootdev_help_request():
+    """This generates a help request for https://boot.dev/ lessons
+    based on their provided formatting in Discord."""
+    lesson_link = input("\nLesson link: ")
+
+    print("\nNow, enter your text (press Ctrl+D to finish):\n")
+    multiline_input = sys.stdin.read()
+    clear_terminal()
+    print(f"{lesson_link}\n\n{multiline_input}")
+    return multiline_input
+
+
+def theodinproject_help_request():
+    """This generates a help request for https://theodinproject.com/ lessons
+    based on their provided formatting in Discord and website."""
+    lesson_link = input("\nLesson link: ")
+
+    print("\nNow, enter your text (press Ctrl+D to finish):\n")
+    multiline_input = sys.stdin.read()
+    clear_terminal()
+    print(f"{lesson_link}\n\n{multiline_input}")
+    return multiline_input
+
+
+def main():
+    """Calls for the user to make a choice on what
+    function they want to call based on available options."""
+    while True:
+        print("Choose a function to execute: ")
+        print("1. Boot.Dev help request")
+        print("2. TheOdinProject help request")
+        print("3. Exit")
+
+        choice = input("\nEnter your choice of 1, 2, or 3: ")
+
+        if choice == "1":
+            clear_terminal()
+            multiline_input = bootdev_help_request()
+            with open("bootdev.md", "w") as file:
+                file.write(multiline_input)
+            break
+        elif choice == "2":
+            clear_terminal()
+            multiline_input = theodinproject_help_request()
+            with open("theodinproject.md", "w") as file:
+                file.write(multiline_input)
+            break
+        elif choice == "3":
+            clear_terminal()
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
