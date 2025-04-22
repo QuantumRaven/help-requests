2fd32546d1365d2bf12199b3807be37af8a5d675
QuantumRaven <carpenterrchloe@protonmail.com>
2025-04-22 12:59:53 -0400

    Remve lines based multiline input
    Swap out previous lines output for use of sys and os modules to simplify program.

--- boot_dev_help_request.py
+++ boot_dev_help_request.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
+
+def clear_terminal():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system("clear")
 
 def help_request():
     lesson_link = input("\nLesson link: ")    
 
     print("\nNow, enter your text (type 'END' on a new line to finish):\n")
-    lines = []
-    while True:
-        line = input()
-        if line == "END":
-            break
-        lines.append(line)
-    text = "\n".join(lines)
-    return lesson_link, text
+    multiline_input = sys.stdin.read()
+    clear_terminal()
+    print(f"{lesson_link}\n\n{multiline_input}")
 
-lesson_link, text = help_request()
-output = f"Lesson link: {lesson_link}\n\n{text}"
-print(output)
+help_request()
